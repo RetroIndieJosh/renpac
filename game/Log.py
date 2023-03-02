bffe511a1c3faec82c3fd3e9b2a2c85fed3cce1d
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-02 16:07:42 -0500

    Add Doxyfile and start documenting Log.

--- game/Log.py
+++ game/Log.py
@@ -5,10 +5,18 @@ from os import getcwd
 from datetime import datetime
 
 class Log:
+    ## the log filename, stored so we can write raw messages to it
     _filename = ""
 
     @staticmethod
     def init(filename):
+        """! Initialize the log. Only one log can be initialized at a time. Any
+        calls to `logging` methods will refer to this log.
+
+        @param filename The filename where the log will be stored. For now, this
+        is in the Ren'py executable directory. Initializing the log will print
+        the full path to the log to the terminal.
+        """
         Log._filename = filename
 
         logging.basicConfig(
@@ -19,10 +27,16 @@ class Log:
         )
 
         Log.write_header("RenPaC Log", timestamp=True)
-        logging.info(f"Started log at {getcwd()}/{filename}")
+        print(f"Started log at {getcwd()}/{filename}")
 
     @staticmethod
     def write_header(message, timestamp=False):
+        """! Write a header to the log. This will be distinguished with a line
+        of hashes and a blank line before and after the header.
+
+        @param message The message to write to the file.
+        @param timestamp If True, write a timestamp before the main header text.
+        """
         Log.write(timestamp=False)
         Log.write_header_line()
         Log.write(message, timestamp)
