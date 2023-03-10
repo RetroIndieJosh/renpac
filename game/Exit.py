e7a1ffec75e2311ef97ff25baa8e5dcf9fc5844c
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-10 11:06:16 -0500

    Resolve #41 - document Exit

--- game/Exit.py
+++ game/Exit.py
@@ -1,12 +1,20 @@
 from . import Action, Hotspot
 
 class Exit(Hotspot):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
-        self.target = None
-        self.width = 0
-        self.height = 0
+    """ A hotspot that leads to another room through "go" action
+    """
 
+    def __init__(self, name: str) -> None:
+        """ Create an Exit
+
+        @param name The unique name (handle) for the exit
+        """
+        super().__init__(name)
+
+        ## The room we "go" to when this exit is activated
+        self.target = None
+
+        # init actions inherited from Hotspot
         self.action_left = Action.get("go")
         self.action_right = None
         self.action_middle = Action.get("examine")
