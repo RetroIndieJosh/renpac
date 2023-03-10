f6f9d0f5777e69f9ccc66a6a851b3863a6d5061f
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-10 11:10:18 -0500

    Resolve #42 - document Game

--- game/Game.py
+++ game/Game.py
@@ -3,16 +3,26 @@ import logging
 from . import Inventory, Room, StaticClass
 
 class Game(StaticClass):
+    """! General game functionality (static class)
+    """
+
+    ## The room where the game starts
     _first_room: Room = None
+
+    ## Whether game data is loaded
     _loaded: bool = False
 
+    # TODO use name to load_game from appropriate file (generator will need
+    # update to call it load_game_{name} instead)
     @staticmethod
     def load(name: str) -> None:
-        logging.info(f"load game '{name}'")
+        """! Load game data 
+        """
+        logging.info(f"loading game")
         Inventory.clear()
 
-        # defined by the gamebuilder
         try:
+            # defined by the gamebuilder
             Game._first_room = load_game() #type: ignore
         except NameError:
             raise Exception("load_game() or a symbol within not found (did you run the gamebuilder?)")
@@ -21,6 +31,8 @@ class Game(StaticClass):
 
     @staticmethod
     def start() -> None:
+        """! If the game is loaded, start it by going to the first room
+        """
         if not Game._loaded:
             raise Exception("cannot start game, not loaded")
         logging.info(f"start game in '{Game._first_room.name}'")
