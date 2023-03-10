51138f1e8245211076906705ce65cf25e47ef562
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-10 13:50:49 -0500

    Resolve #50 - document StaticClass

--- game/StaticClass.py
+++ game/StaticClass.py
@@ -1,3 +1,7 @@
 class StaticClass:
+    """! Prevent instatiation of a class that inherits this.
+    """
     def __init__(self) -> None:
+        """! Prevent an instance of the class from being created
+        """
         raise Exception("Cannot instantiate a static class")
