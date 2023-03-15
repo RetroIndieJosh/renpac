4468fbf6ad9f560b071a1c61d697d8bf2a2f0fec
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-14 20:17:17 -0400

    Move target constants to Item

--- game/renpac-engine/Item.py
+++ game/renpac-engine/Item.py
@@ -1,6 +1,13 @@
 import logging
 
-from Combination import *
+## Flag for no target
+TARGET_NONE = 0b00
+
+## Flag for targeting the selected item (the one the player is using on a target hotspot)
+TARGET_SELF = 0b01
+
+## Flag for targeting the target item (the hotspot the player "uses" the selected item on)
+TARGET_OTHER = 0b10
 
 from . import Cursor, Hotspot, Action, Combination, Renpac
 
