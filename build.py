5036b2019d00d2b2076a0c426f9ce249b13318f2
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-15 14:23:11 -0400

    Close #15 - skip unchanged files when copying

--- build.py
+++ build.py
@@ -1,9 +1,12 @@
+import filecmp
 import platform
 import shutil
 import subprocess
 
 from configparser import ConfigParser
 from Path import Path
+
+from printv import *
 
 from combos import *
 from exits import *
@@ -13,6 +16,17 @@ from rooms import *
 from Config import Config
 from Game import Game
 from Script import Script
+
+import argparse
+parser = argparse.ArgumentParser()
+ 
+parser.add_argument("-f", "--force", action='store_true', help = "Force Overwrite: Copy all files, even if they match the destination.")
+parser.add_argument("-v", "--verbose", action='store_true', help = "Verbose Mode: Show additional process messages")
+ 
+args = vars(parser.parse_args())
+FORCE_OVERWRITE = args['force']
+if args['verbose']:
+    enable_verbose()
 
 THIS_PATH = os.path.dirname(__file__)
 
@@ -26,19 +40,21 @@ class Build:
         self._audio_path = None
 
     def build(self) -> None:
+        print(f"Building {self._game_name}")
         self.parse_config()
-        self.print()
+        self.printv()
         self.clean()
         self.build_engine()
         self.copy_engine()
         self.build_game()
         self.copy_resources()
+        print(f"Build done")
 
     def build_engine(self) -> None:
         builder = "generate.bat" if platform.system() == "Windows" else "./generate.sh"
         engine_path = self._engine_path.get()
 
-        print(f"building engine: {engine_path}/{builder}")
+        printv(f"building engine: {engine_path}/{builder}")
         os.chdir(engine_path)
         subprocess.run(builder)
         os.chdir(THIS_PATH)
@@ -63,12 +79,12 @@ class Build:
         Game.finalize()
 
         path = self._output_file_path
-        print(f"writing game file to '{path}'")
+        printv(f"writing game file to '{path}'")
         Script.write_file(path.get())
 
     def clean(self) -> None:
         path = self._output_path.get()
-        print(f"cleaning '{path}'")
+        printv(f"cleaning '{path}'")
         shutil.rmtree(path, ignore_errors=True)
 
     def copy_engine(self) -> None:
@@ -77,11 +93,32 @@ class Build:
         # engine builder script
         source_path = Path(f"{self._engine_path}/game").get()
         dest_path = self._output_path.get()
-        print(f"copying engine from '{source_path}' to '{dest_path}'")
+        printv(f"copying engine from '{source_path}' to '{dest_path}'")
+        self.copy_files(source_path, dest_path)
         # TODO if building for release, include .rpyc and but exclude .rpy
-        shutil.copytree(source_path, dest_path,
-                        ignore=shutil.ignore_patterns("renpac-engine", "*.py", "*.pyc", "*.rpyc", "*.bak"),
-                        copy_function=shutil.copy2)
+        #shutil.copytree(source_path, dest_path, 
+                        #ignore=shutil.ignore_patterns("renpac-engine", "*.py", "*.pyc", "*.rpyc", "*.bak"),
+                        #copy_function=shutil.copy2)
+
+    def copy_files(self, source_dir: str, dest_dir: str, relative_dir: str = ""):
+        dir = os.path.join(source_dir, relative_dir)
+        for file in os.listdir(dir):
+            source_file = os.path.join(source_dir, relative_dir, file)
+            if os.path.isdir(source_file):
+                self.copy_files(source_dir, dest_dir, os.path.join(relative_dir, file))
+            else:
+                printv(f"[{relative_dir}] ", end='')
+                dest_file = os.path.join(dest_dir, relative_dir, file)
+                if not FORCE_OVERWRITE:
+                    # skip copy if the destination file matches exactly
+                    if os.path.exists(dest_file):
+                        if os.path.getsize(source_file) == os.path.getsize(dest_file):
+                            if filecmp.cmp(source_file, dest_file, False):
+                                printv(f"SKIP\n\t{source_file}")
+                                continue
+                printv(f"COPY\n\t{source_file}\n\tTO {dest_file}")
+                os.makedirs(os.path.dirname(dest_file), exist_ok=True)
+                shutil.copy2(source_file, dest_file)
 
     def copy_resources(self) -> None:
         mapping = {
@@ -91,11 +128,12 @@ class Build:
         for source_path in mapping:
             resource_type = mapping[source_path]
             dest_path = Path(f"{self._output_path}/{resource_type}", False)
-            print(f"copying resources ({resource_type}) from '{source_path}' to '{dest_path}'")
+            printv(f"copying resources ({resource_type}) from '{source_path}' to '{dest_path}'")
+            self.copy_files(source_path.get(), dest_path.get())
             # TODO figure out what ignores we want for this (maybe none? or customizable in build config?)
-            shutil.copytree(source_path.get(), dest_path.get(),
+            #shutil.copytree(source_path.get(), dest_path.get(),
                             #ignore=shutil.ignore_patterns("renpac-engine", "*.py", "*.pyc", "*.rpyc", "*.bak"),
-                            copy_function=shutil.copy2)
+                            #copy_function=shutil.copy2)
 
     def parse_config(self) -> None:
         parser = ConfigParser()
@@ -138,8 +176,8 @@ class Build:
         self._output_path = Path(f"build/{self._game_name}/game", False)
         self._output_file_path = Path(f"{self._output_path}/{self._game_name}.game.rpy", False)
 
-    def print(self) -> None:
-        print(f"Paths:"
+    def printv(self) -> None:
+        printv(f"Paths:"
               f"\n\tEngine: '{self._engine_path}'"
               f"\n\tFiles: '{self._game_path}'"
               f"\n\tGame Config: '{self._game_config_path}'"
