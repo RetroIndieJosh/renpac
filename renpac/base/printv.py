f3dfb9546d3fd7a397628c157fe4f48035096f43
Joshua McLean <mrjoshuamclean@gmail.com>
2023-03-18 11:35:46 -0400

    Add test to printv and fix newline doubling

--- renpac/base/printv.py
+++ renpac/base/printv.py
@@ -1,9 +1,34 @@
 verbose = False
 
+def main():
+    print("[Testing printv, no verbose]")
+    printv("+ This is a message that should not appear")
+    print("+ This is a message that should appear")
+    print("+ There should be two messages starting with +")
+
+    print("[Testing printv, verbose]")
+    enable_verbose()
+    print("* Verbose mode is now enabled")
+    printv("* This should appear")
+    print("* There should be three messages starting with *")
+
+    print("[Testing printv end]")
+    printv("This message should run on", end=' ')
+    printv("to this message")
+    printv("and this should be on a new line")
+    printv("This message should", end=" not ")
+    printv("end with a word", end='?')
+    printv()
+    printv()
+    printv("and this should have a blank line above it")
+
 def enable_verbose():
     global verbose
     verbose = True
 
-def printv(msg, end='\n'):
+def printv(msg="", end='\n'):
     if verbose:
-        print(msg, end)
+        print(msg, end=end)
+
+if __name__ == "__main__":
+    main()
