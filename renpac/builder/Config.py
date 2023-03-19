1a73cbcb8aa8166d461b08c2a61e56a9ac067b4a
Retro Indie Josh <mrjoshuamclean@gmail.com>
2023-03-19 12:27:55 -0400

    Improved key message and name handling for config

--- renpac/builder/Config.py
+++ renpac/builder/Config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Callable, Dict, List
 
 from renpac.base.printv import *
@@ -14,9 +15,8 @@ class ConfigEntry:
 # make non-static; Game can contain the game config, Build can contain the build
 # config
 class Config:
+    _name: str = None
     _parser: ConfigParser = None
-
-    filename: str = None
 
     @staticmethod
     def get_section(section_key: str) -> list:
@@ -29,11 +29,11 @@ class Config:
         return Config._parser[section_key]
 
     @staticmethod
-    def load(config_filename: str) -> None:
+    def load(config_path: str) -> None:
         Config._parser = ConfigParser()
-        Config.filename = config_filename
-        if len(Config._parser.read(Config.filename)) == 0:
-            raise Exception(f"could not open or no data in game config '{Config.filename}'")
+        if len(Config._parser.read(config_path)) == 0:
+            raise Exception(f"could not open or no data in game config '{config_path}'")
+        Config._name = Path(config_path).name
 
     @staticmethod
     def sections() -> list:
@@ -41,6 +41,10 @@ class Config:
             printv("ERROR: config not loaded, cannot get sections")
             return None
         return Config._parser.sections()
+
+    @staticmethod
+    def key_message(key, section_name) -> str:
+        return f"key '{key}' in section '{section_name}' of '{Config._name}'"
 
     @staticmethod
     def parse_section(section_name: str, entries: List[ConfigEntry]) -> Dict[str, str]:
@@ -51,11 +55,11 @@ class Config:
                 values[key] = section[key]
                 # TODO type validation
             else:
-                print(f"WARNING unknown '{section_name}' key '{key}'")
+                print(f"WARNING unknown {Config.key_message(key, section_name)}")
         for key in entries:
             if key not in values:
                 if entries[key].is_required:
-                    raise Exception(f"ERROR missing required key '{key}' in section '{section_name}'")
+                    raise Exception(f"ERROR missing required {Config.key_message(key, section_name)}")
                 else:
-                    print(f"WARNING missing optional key '{key}' in section '{section_name}'")
+                    print(f"WARNING missing optional {Config.key_message(key, section_name)}")
         return values
